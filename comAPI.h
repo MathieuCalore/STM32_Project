6696ac9c8d51d8da5cef6b74c9968492c34ecb7a
Cyril Hartmann <145751737+Cyril-HARTMANN@users.noreply.github.com>
2023-12-08 14:37:06 +0100

    First commit (yay!) API communication UART

---
+++ comAPI.h
@@ -0,0 +1,30 @@
+/*
+ * comAPI.h
+ *
+ *  Created on: Dec 8, 2023
+ *      Author: zone107
+ */
+
+#ifndef INC_COMAPI_H_
+#define INC_COMAPI_H_
+
+uint8_t syscodeTransmitBuffer[2];
+uint8_t syscodeReceiveBuffer[2];
+char payloadBuffer[256];
+
+
+void transmitSyscodeForward(uint8_t* syscodeToTransmit);
+void transmitSyscodeBackward(uint8_t* syscodeToTransmit);
+void receiveSyscodeFromNext(uint8_t* syscodeToReceive);
+void receiveSyscodeFromPrevious(uint8_t* syscodeToReceive);
+void transmitPayloadForward(char* payloadToTransmit,uint8_t payloadSize);
+void transmitPayloadBackward(char* payloadToTransmit,uint8_t payloadSize);
+void receivePayloadFromNext(char* payloadToReceive,uint8_t payloadSize);
+void receivePayloadFromPrevious(char* payloadToReceive,uint8_t payloadSize);
+void syscodeBufferCoder(uint8_t syscode,uint8_t payloadSize);
+void syscodeBufferDecoder(uint8_t* syscodeBuffer);
+
+void transmitMessageToPc(char* message,uint8_t messageSize);
+void receiveMessageFromPc(char* message,uint8_t messageSize);
+
+#endif /* INC_COMAPI_H_ */
