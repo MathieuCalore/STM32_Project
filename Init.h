95bbf89a978c76b47911c0e6a335c0df8dde06d3
Jules Oulhen <oulhenjule@cy-tech.fr>
2023-12-08 14:50:41 +0100

    Init library

---
+++ Init.h
@@ -0,0 +1,17 @@
+/*
+ * Init.h
+ *
+ *  Created on: Dec 8, 2023
+ *      Author: oulhenjule
+ */
+
+#ifndef INC_INIT_H_
+#define INC_INIT_H_
+
+#include "comAPI.h"
+
+void Init_Sequence();
+
+
+
+#endif /* INC_INIT_H_ */
